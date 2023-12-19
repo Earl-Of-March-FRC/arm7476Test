e002782413195872d2b1a7d3c24051091db0266d
Sadra-Hemmati <shemm1@ocdsb.ca>
2023-12-19 16:37:55 -0500

    Arm ready to test, but no encoders

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,10 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ArmCommand;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -21,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ArmSubsystem armSubsystem = new ArmSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -30,6 +33,11 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    armSubsystem.setDefaultCommand(new ArmCommand(
+      armSubsystem,
+      () -> m_driverController.getLeftY(),
+      () -> m_driverController.getRightY()
+    ));
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/ArmCommand.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSubsystem;
+import java.util.function.Supplier;
+
+public class ArmCommand extends CommandBase {
+  /** Creates a new ArmCommand. */
+  private ArmSubsystem Arm;
+  private Supplier<Double> speedSupplier, inclineSupplier;
+  
+  public ArmCommand(ArmSubsystem Arm, Supplier<Double> speedSupplier, Supplier<Double> inclineSupplier){
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.Arm = Arm;
+    this.speedSupplier = speedSupplier;
+    this.inclineSupplier = inclineSupplier;
+    addRequirements(Arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Arm.extend(speedSupplier.get());
+    Arm.incline(inclineSupplier.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
